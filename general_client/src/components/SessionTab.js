df7691d96b6b86154a93ea823bdd6f882ae44ae7
afaan1000 <saadafaan.syed@mail.utoronto.ca>
2020-03-13 23:53:07 -0400

    NOX-31: add dropdown arrow for sessions list

--- general_client/src/components/SessionTab.js
+++ general_client/src/components/SessionTab.js
@@ -1,3 +1,212 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+// import SessionTab  from "./SessionTab";
+import { connect } from "react-redux";
+import {
+  //getCourses,
+  getSessions,
+  downloadSession
+} from "../actions/sessionActions";
+import PropTypes from "prop-types";
+import Cookies from "universal-cookie";
+
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import AddIcon from "@material-ui/icons/AddBox";
+import DownloadIcon from "@material-ui/icons/SaveAlt";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import IconButton from "@material-ui/core/IconButton";
+import SessionItemModal from "./SessionItemModal";
+import toggle from "./SessionItemModal";
+
+import { makeStyles } from "@material-ui/core/styles";
+//import ListSubheader from "@material-ui/core/ListSubheader";
+//import List from "@material-ui/core/List";
+//import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+//import ListItemText from "@material-ui/core/ListItemText";
+//import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+//import ExpandLess from "@material-ui/icons/ExpandLess";
+//import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+
+const cookies = new Cookies();
+
+class SessionTab extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pid: cookies.get("pid") || "Furki"
+    };
+
+    // this.changeBtnValue = this.changeBtnValue.bind(this);
+  }
+
+  static propTypes = {
+    getSessions: PropTypes.func.isRequired,
+    session: PropTypes.object.isRequired
+    //getCourses: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    console.log(this.state.pid);
+    this.props.getSessions(this.state.pid);
+    //console.log(getCourses(this.state.pid));
+  }
+
+  onDownloadClick(session) {
+    this.props.downloadSession(session);
+  }
+  // onAddSessionClick() {
+  //   this.props.toggle();
+  // }
+
+  // changeBtnValue() {
+  //   const newCourse = {
+  //     pid: PID, //Get from cookies once authentication is up and running
+  //     courseCode: this.course
+  //   };
+
+  //   this.props.addCourse(newCourse);
+  // }
+
+  render() {
+    const { sessions } = this.props.session;
+    console.log(this.props.session);
+    return (
+      <Container>
+        <ListGroup>
+          <TransitionGroup className="sessions-list">
+            {sessions.map(session => (
+              <CSSTransition timeout={500}>
+                <ListItem>
+                  <ListItemText primary={session} />
+                  <Button
+                    color="dark"
+                    style={{ marginBottom: "2rem" }}
+                    onClick={this.onDownloadClick.bind(this, session)}
+                  >
+                    Download Session Data
+                  </Button>
+                </ListItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  session: state.session
+});
+
+export default connect(mapStateToProps, {
+  //getCourses,
+  getSessions,
+  downloadSession
+})(SessionTab);
+
+/*
+import React, { useState } from 'react';
+import { ListGroup } from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import AddIcon from '@material-ui/icons/AddBox';
+import DownloadIcon from '@material-ui/icons/SaveAlt';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import { connect } from 'react-redux';
+import { getSessions, getCourses, downloadSession } from '../actions/sessionActions';
+import PropTypes from 'prop-types';
+
+var courses = ["CSC343H5", "STA256H5", "CSC258H5"];
+var sessions = ["Week 1 - LEC0102", "Week 2 - LEC0102"];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function NestedList(props) {
+
+  const [course, setCourse] = useState('Default');
+  const [session, setSession] = useState('Default');
+  const [pid, setPid] = useState('Furki');
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Larry's Courses
+      </ListSubheader>
+      }
+      className={classes.root}
+    >
+      {
+        courses.map(course =>
+          //For each "course" in courses_array, do the following:
+          <ListGroup>
+            {
+              <ListItem button onClick={handleClick}>
+                <IconButton className={classes.button} aria-label="add">
+                  <AddIcon />
+                </IconButton>
+                <ListItemText primary={course} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+            }
+            {course === "CSC343H5" ?
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding> {
+                  sessions.map(session =>
+                    <ListItem button className={classes.nested}>
+                      <IconButton
+                        className={classes.button}
+                        aria-label="download">
+                        <DownloadIcon />
+                      </IconButton>
+                      <ListItemText primary={session} />
+                    </ListItem>
+                  )}
+                </List>
+              </Collapse>
+              : null}
+          </ListGroup>)}
+    </List >
+  );
+}
+*/
+
 // import React, { Component } from "react";
 // import { Container, ListGroup, ListGroupItem, Button} from "reactstrap";
 // import { CSSTransition, TransitionGroup } from "react-transition-group";
@@ -24,8 +233,6 @@
 // const cookies = new Cookies();
 // const PID = cookies.get("pid") || "Furki";
 
-
-
 // class SessionTab extends Component{
 //     constructor(props){
 //         super(props);
@@ -37,7 +244,7 @@
 //         pid: PropTypes.object.isRequired
 //     };
 
-//     //gets the course that is 
+//     //gets the course that is
 //     componentDidMount() {
 //         console.log(this.props.sesid);
 //         //this.props.getCourses(this.state.sesid);
@@ -74,15 +281,13 @@
 //         </Container>)
 //     }
 
-    
 // }
 // const mapStateToProps = state => ({
 //     session: state.session
 //   });
-  
+
 // export default connect(mapStateToProps, {
 //     getCourses,
 //     addCourse,
 //     downloadSession
 // })(SessionTab);
-
--- general_client/src/components/SessionsList.js
+++ general_client/src/components/SessionsList.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import { Container, ListGroup, ListGroupItem, Button} from "reactstrap";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 // import SessionTab  from "./SessionTab";
 import { connect } from "react-redux";
-import {
-  getSessions,
-  downloadSession
-} from "../actions/sessionActions";
+import { getSessions, downloadSession } from "../actions/sessionActions";
 import { getCourses } from "../actions/courseActions";
 import PropTypes from "prop-types";
 import Cookies from "universal-cookie";
@@ -17,12 +14,13 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
 import AddIcon from "@material-ui/icons/AddBox";
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import DownloadIcon from "@material-ui/icons/SaveAlt";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import IconButton from "@material-ui/core/IconButton";
 import SessionItemModal from "./SessionItemModal";
+import SessionTab from "./SessionTab";
 
 const cookies = new Cookies();
 
@@ -31,7 +29,8 @@ class SessionsList extends Component {
     super(props);
 
     this.state = {
-      pid: cookies.get("pid") || "Furki"
+      pid: cookies.get("pid") || "Furki",
+      open: false
     };
 
     // this.changeBtnValue = this.changeBtnValue.bind(this);
@@ -52,22 +51,28 @@ class SessionsList extends Component {
 
   onDownloadClick(course) {
     this.props.downloadSession(course);
-  };
+  }
 
   // changeBtnValue(course) {
   //   const newSession = {
   //     pid: PID, //Get from cookies once authentication is up and running
   //     courseCode: course
   //   };
-    
+
   //   this.props.addSession(newSession);
   // }
 
   //   this.props.addCourse(newCourse);
   // }
-  
+
+  toggleOpen = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
   render() {
-    const { sessions } = this.props.session;
+    //const { sessions } = this.props.session;
     const { courses } = this.props.course;
     console.log(this.props.session);
     return (
@@ -76,25 +81,31 @@ class SessionsList extends Component {
           <TransitionGroup className="sessions-list">
             {courses.map(course => (
               <CSSTransition timeout={500}>
-                <ListItem >
-                  <SessionItemModal course={course}/>
-                   {/*<IconButton
+                <List>
+                  <ListItem button onClick={this.toggleOpen}>
+                    <SessionItemModal course={course} />
+                    {/*<IconButton
                     aria-label="add"
                     key={course}
                     onClick={this.changeBtnValue.bind(this, course)}
                   >
                     <AddIcon />
                   </IconButton>*/}
-                  <ListItemText primary={course}/>
-                  <IconButton
+                    <ListItemText primary={course} />
+                    <IconButton
                       color="dark"
                       style={{ marginBottom: "2rem" }}
-                      onClick = {this.onDownloadClick.bind(this, course)}
-                      >
+                      onClick={this.onDownloadClick.bind(this, course)}
+                    >
                       <ArrowDownwardIcon />
-                  </IconButton>
-                </ListItem>
+                    </IconButton>
 
+                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  {/* <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <SessionTab />
+                  </Collapse> */}
+                </List>
               </CSSTransition>
             ))}
           </TransitionGroup>
@@ -103,7 +114,7 @@ class SessionsList extends Component {
     );
   }
 }
-  
+
 const mapStateToProps = state => ({
   session: state.session,
   course: state.course
@@ -111,6 +122,6 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, {
   getCourses,
-  getSessions,
+  //getSessions,
   downloadSession
 })(SessionsList);
